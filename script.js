14823537cdf72768e81a5064648838c88731c0d5
M. Manousos <mmanousos@gmail.com>
2017-11-27 16:41:57 -0800

    base documents

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $("#search").on("click", function() {
+        $(".hidden").slideToggle();
+    })
+    
+    
+    var url = 'https://en.wikipedia.org/wiki/Special:Random';
+    
+}); 
